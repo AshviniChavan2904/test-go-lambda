38108c8c4d1594f6d256d4fce8796eb9b7d5ab1e
Ashvini Chavan <AChavan@TechStyle.com>
2024-06-13 16:59:23 +0530

    ignore changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,7 +35,8 @@ resource "aws_iam_role" "lambda_role" {
   })
   lifecycle {
     ignore_changes = [
-      name,assume_role_policy // Ignore changes to the assume role policy
+      "name",
+      "assume_role_policy" // Ignore changes to the assume role policy
     ]
   }  
 }
