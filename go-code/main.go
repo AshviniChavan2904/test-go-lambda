7de095dc47cf1cd25cfeecc0f927b0618af8e2c6
Ashvini Chavan <AChavan@TechStyle.com>
2024-06-13 16:50:22 +0530

    changed go code

--- go-code/main.go
+++ go-code/main.go
@@ -1,21 +1,9 @@
 package main
 
 import (
-	"context"
-	"github.com/aws/aws-lambda-go/events"
-	"github.com/aws/aws-lambda-go/lambda"
-	"log"
+    "fmt"
 )
 
-// Handler is your Lambda function handler
-func Handler(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	log.Printf("Received request: %+v", event)
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello from Lambda!",
-	}, nil
-}
-
 func main() {
-	lambda.Start(Handler)
+    fmt.Println("Hello, World!")
 }
